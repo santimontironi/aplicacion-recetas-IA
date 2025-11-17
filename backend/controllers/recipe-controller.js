b333fd158e1a31cbb8f6cdb73872731b4f1d40b3
santimontironi <santiimontironi@gmail.com>
2025-11-17 17:41:44 -0300

    nuevas funciones en los controladroes

--- backend/controllers/recipe-controller.js
+++ backend/controllers/recipe-controller.js
@@ -11,9 +11,35 @@ export const addRecipe = async (req, res) => {
             user: req.user.id
         });
 
-        res.json({ recipe });
+        res.status(200).json({ message: 'Receta agregada correctamente', newRecipe: recipe });
     }
     catch (error) {
         return res.status(500).json({ message: 'Error al agregar la receta', error: error.message });
+    }
+}
+
+export const allRecipes = async (req,res) => {
+    try{
+        const userId = req.user.id
+
+        const recipes = await Recipe.find({ user: userId });
+
+        res.status(200).json({ recipes });
+    }
+    catch(error){
+        res.status(500).json({message: 'Error al obtener las recetas', error: error.message});
+    }
+}
+
+export const deleteRecipe = async (req,res) => {
+    try{
+        const recipeId = req.params.id;
+
+        await Recipe.findByIdAndUpdate(recipeId, { active: false });
+
+        res.status(200).json({ message: 'Receta eliminada correctamente' });
+    }
+    catch(error){
+        res.status(500).json({message: 'Error al eliminar la receta', error: error.message});
     }
 }
--- backend/models/recipe-model.js
+++ backend/models/recipe-model.js
@@ -7,6 +7,7 @@ mongoose.Schemas.Recipe = new mongoose.Schema({
     time: { type: String, required: true },
     difficulty: { type: String, required: true },
     date: { type: Date, required: true, default: Date.now() },
+    active: { type: Boolean, required: true, default: true },
     user: { type: String, required: true }
 });
 
