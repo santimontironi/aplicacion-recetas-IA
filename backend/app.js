ac333a495e558acdfa66336a32244562437c7f47
santimontironi <santiimontironi@gmail.com>
2025-12-10 19:43:22 -0300

    config cors

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+app.options("*", cors());
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
