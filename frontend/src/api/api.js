525535fd38d0f94515e7b36fb4a3377752e6d20f
santimontironi <santiimontironi@gmail.com>
2026-03-07 20:27:39 -0300

    .env agregado

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseUrl = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL_PROD,
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true
 })
 
